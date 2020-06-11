83e5ea7a8f921ab7509b355ea26b8be557c631c8
vu <talkingboutlove115>
2020-06-11 14:01:02 +0700

    first commit

---
+++ src/main/java/news/controller/web/HomeController.java
@@ -0,0 +1,29 @@
+package news.controller.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = {"/trang-chu"})
+public class HomeController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/views/index.jsp");
+		rd.forward(request, response);
+	
+	}
+
+}
